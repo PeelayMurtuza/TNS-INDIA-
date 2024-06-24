06f6a89004bd0fe31e3cb625d1a2d13ad4f34279
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-06-24 15:54:05 +0530

    first project

---
+++ FirstProj.java
@@ -0,0 +1,9 @@
+package TNSIndiaFoundJava;
+
+public class FirstProj {
+    public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+    }
+}
